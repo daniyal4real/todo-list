f665a141483e000eaea86f94ea930ebcd26889ad
Daniyal <56621752+daniyal4real@users.noreply.github.com>
2023-01-23 17:20:55 +0600

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,92 @@
+//
+//  ViewController.swift
+//  KeepInMind
+//
+//  Created by Daniyal Ganiuly on 23.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+
+    
+
+    
+    private let table: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    var items = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Keep In Mind"
+        
+        view.addSubview(table)
+        table.dataSource = self
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
+    
+        
+        self.items = UserDefaults.standard.stringArray(forKey: "items") ?? []
+    
+    }
+    
+    @objc private func didTapAdd() {
+        let alert = UIAlertController(title: "Новая запись", message: "Введите новую запись",
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Отменить", style: .cancel,
+                                      handler: nil))
+        alert.addAction(UIAlertAction(title: "Завершить", style: .default,
+                                      handler: { [weak self] (_) in
+            if let field = alert.textFields?.first {
+                if let text = field.text, !text.isEmpty {
+                    
+                    
+                    DispatchQueue.main.async {
+                        var currentItems = UserDefaults.standard.stringArray(forKey: "items") ?? []
+                        
+                        currentItems.append(text)
+                        
+//                        let newEntry = [text]
+                        UserDefaults.standard.setValue(currentItems, forKey: "items")
+                        self?.items.append(text)
+                        self?.table.reloadData()
+                    }
+                    
+                }
+            }
+        }))
+        
+        alert.addTextField { field in
+            field.placeholder = "Введите запись..."
+        }
+        present(alert, animated: true)
+    }
+    
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        table.frame = view.bounds
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+        
+    }
+
+
+}
+
